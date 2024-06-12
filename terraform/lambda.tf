f5cd3903f58b494308fc72b5a0fe92c73d083dd6
Niall <niallcanning@icloud.com>
2024-06-12 14:09:45 +0100

    workflow fix (tf bucket error)

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -7,32 +7,12 @@ provider "aws" {
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
-resource "aws_s3_bucket" "lambda_code" {
-  bucket = "dayoff-bucket"  # Update this to your existing bucket name
-}
-
 resource "aws_lambda_function" "dayoff_lambda" {
   function_name = "dayoff"
-  s3_bucket     = aws_s3_bucket.lambda_code.bucket
+  s3_bucket     = "dayoff-bucket"  # Directly use the existing bucket name
   s3_key        = "lambda/zipped_functions/dayoff-lambda.zip"
   handler       = "main.handler"
   runtime       = "python3.9"
-  role          = aws_iam_role.lambda_exec.arn
+  role          = "arn:aws:iam::715139502280:role/lambda_exec_role"  # Use the existing role ARN
 }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "lambda_exec_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
