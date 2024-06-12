035e307e81054c6a2641255c763d8ae86144a146
Niall <niallcanning@icloud.com>
2024-06-12 13:49:28 +0100

    workflow fix (tf credentials error)

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,13 +1,18 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
 resource "aws_s3_bucket" "lambda_code" {
   bucket = "dayoff-bucket"
 }
 
 resource "aws_lambda_function" "dayoff_lambda" {
-  function_name = "dayoff"  # Corrected Lambda function name
+  function_name = "dayoff"
 
   s3_bucket = aws_s3_bucket.lambda_code.bucket
   s3_key    = "lambda/zipped_functions/dayoff-lambda.zip"
